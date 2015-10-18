bcb46d73343003033493d51ebe2b63494365574f
Peter Sagerson <psagers.hg@ignorare.net>
2015-10-18 11:39:09 -0700

    Build tools.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='salvage',
-    version='0.1.1',
+    version='0.1.3',
     description='Allows a group of people to hold sensitive information using a simple secret-splitting scheme.',
     long_description=open('README.rst').read(),
     author='Peter Sagerson',
@@ -16,14 +16,15 @@ setup(
     license='BSD',
     install_requires=[],
     classifiers=[
-        "Development Status :: 4 - Beta",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.2",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "License :: OSI Approved :: BSD License",
-        "Topic :: Security",
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Security',
     ],
 )
